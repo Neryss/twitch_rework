0081eccb9b997fdca9429903be84913ecdc4b382
neryss <neryssistos@gmail.com>
2023-10-18 17:56:03 +0200

    cleaned up a bit

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const   WebSocket = require('ws');
 // const   ws = new WebSocket("wss://eventsub.wss.twitch.tv/ws");
 const   ws = new WebSocket("ws://localhost:8080/ws");
 const   cron = require('node-cron');
+const   twitch_ft = require('./srcs/twitch');
 
 cron.schedule('* * */2 * *', () => {
     if (app_token)
-        refresh_token(app_token);
+        twitch_ft.refresh_token(app_token);
 });
 
 let   init = true;
@@ -39,77 +40,15 @@ ws.on('message', (msg) => {
         if (parsed_msg.payload.subscription.type == "stream.online")
         {
             console.log("\x1b[33m Stream Online \x1b[0m");
+            let stream_on = {
+                user_name: parsed_msg.payload.event.broadcaster_user_name
+            }
+            console.log(stream_on);
         }
     }
 })
 
 // Need to get the broadcaster id
-
-async function  subscribe_to_event(id, event, version) {
-    return new Promise((resolve, reject) => {
-        console.log("Session id: " + twitch_message_id);
-        axios({
-            method: "POST",
-            url: "https://api.twitch.tv/helix/eventsub/subscriptions",
-            headers: {
-                "Client-Id": process.env["TWITCH_ID"],
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + app_token.access_token
-            },
-            data: {
-                "type": event,
-                "version": version,
-                "condition": {"broadcaster_user_id": id, "moderator_user_id": id},
-                "transport": {"method": "websocket", "session_id": twitch_message_id}
-            }
-        }).then((data) => {
-            console.log(JSON.stringify(data.data));
-            resolve(data.data);
-        }).catch((error) => {
-            console.log(error.response.data.message);
-            reject(error);
-        })
-    })
-}
-
-async function  get_user() {
-    return new Promise((resolve, reject) => {
-        axios({
-            method: "GET",
-            url: `https://api.twitch.tv/helix/users?login=Neryss002`,
-            headers: {
-                "Client-Id": process.env["TWITCH_ID"],
-                "Content-Type": "application/json",
-                Authorization: "Bearer " + app_token.access_token
-            }
-        }).then((data) => {
-            console.log("User: " + JSON.stringify(data.data));
-            resolve(data.data);
-        }).catch((error) => {
-            console.log("Error: " + error.response.data.message);
-            reject(error.response.data.message);
-        })
-    })
-}
-
-async function  refresh_token(token) {
-    return new Promise((resolve, reject) => {
-        console.log("refreshing token...");
-        axios.post("https://id.twitch.tv/oauth2/token", {
-            client_id: process.env["TWITCH_ID"],
-            client_secret: process.env["TWITCH_SECRET"],
-            grant_type: "refresh_token",
-            refresh_token: token.refresh_token
-        }).then((data) => {
-            console.log("token refreshed, writing to file");
-            // fs.writeFileSync('./.token.json', JSON.stringify(data.data), {encoding: 'utf-8'});
-            resolve(data.data);
-        }).catch((error) => {
-            console.log(error.response.data.message);
-            reject(error);
-        })
-    })
-}
 
 app.get('/', (req, res) => {
     axios.post("https://id.twitch.tv/oauth2/token", {
@@ -139,8 +78,8 @@ async function    main() {
         });
     }, 5000);
     console.log("Token: " + app_token);
-    const   user = await get_user();
-    await   subscribe_to_event(user.data[0].id, "channel.follow", "2");
-    await   subscribe_to_event(user.data[0].id, "stream.online", "1");
-    await   subscribe_to_event(user.data[0].id, "stream.subscribe", "1");
+    const   user = await twitch_ft.get_user(app_token);
+    await   twitch_ft.subscribe_to_event(user.data[0].id, "channel.follow", "2", twitch_message_id, app_token);
+    await   twitch_ft.subscribe_to_event(user.data[0].id, "stream.online", "1", twitch_message_id, app_token);
+    // await   subscribe_to_event(user.data[0].id, "stream.subscribe", "1");
 }
---
+++ srcs/twitch.js
@@ -0,0 +1,67 @@
+const   axios = require('axios').default;
+require('dotenv').config();
+
+module.exports = {
+    subscribe_to_event: (id, event, version, twitch_message_id, app_token) => {
+        return new Promise((resolve, reject) => {
+            console.log("Session id: " + twitch_message_id);
+            axios({
+                method: "POST",
+                url: "https://api.twitch.tv/helix/eventsub/subscriptions",
+                headers: {
+                    "Client-Id": process.env["TWITCH_ID"],
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + app_token.access_token
+                },
+                data: {
+                    "type": event,
+                    "version": version,
+                    "condition": {"broadcaster_user_id": id, "moderator_user_id": id},
+                    "transport": {"method": "websocket", "session_id": twitch_message_id}
+                }
+            }).then((data) => {
+                console.log(JSON.stringify(data.data));
+                resolve(data.data);
+            }).catch((error) => {
+                console.log(error.response.data.message);
+                reject(error);
+            })
+        })
+    },
+    get_user: (app_token) => {
+        return new Promise((resolve, reject) => {
+            axios({
+                method: "GET",
+                url: `https://api.twitch.tv/helix/users?login=Neryss002`,
+                headers: {
+                    "Client-Id": process.env["TWITCH_ID"],
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + app_token.access_token
+                }
+            }).then((data) => {
+                console.log("User: " + JSON.stringify(data.data));
+                resolve(data.data);
+            }).catch((error) => {
+                console.log("Error: " + error.response.data.message);
+                reject(error.response.data.message);
+            })
+        })
+    },
+    refresh_token: (token) => {
+        return new Promise((resolve, reject) => {
+            console.log("refreshing token...");
+            axios.post("https://id.twitch.tv/oauth2/token", {
+                client_id: process.env["TWITCH_ID"],
+                client_secret: process.env["TWITCH_SECRET"],
+                grant_type: "refresh_token",
+                refresh_token: token.refresh_token
+            }).then((data) => {
+                console.log("token refreshed");
+                resolve(data.data);
+            }).catch((error) => {
+                console.log(error.response.data.message);
+                reject(error);
+            })
+        })
+    }
+}
